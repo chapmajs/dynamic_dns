20e6c31fcdfc048cfb7c818369ef998485daa8be
Jonathan Chapman <chapmajs@gmail.com>
2017-01-17 19:56:35 -0500

    Fixing deploy to internal staging server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,11 +1,11 @@
 set :stage, :production
 
-set :default_env, { :path => "$HOME/.gem/ruby/2.2/bin:$PATH" }
+set :default_env, { :path => "$HOME/.gem/ruby/2.3/bin:$PATH" }
 
-server 'services.theglitchworks.net', user: 'services', roles: %w{app db web}, my_property: :my_value
+server 'services.bv.theglitchworks.net', user: 'services', roles: %w{app db web}, my_property: :my_value
 set :unicorn_rack_env, :production
 set :unicorn_config_path, "#{current_path}/config/unicorn.rb"
-set :unicorn_pid, "/var/run/unicorn/site_services.pid"
+set :unicorn_pid, "/var/run/unicorn/dynamic_dns.pid"
 set :linked_files, %w{config/database.yml}
 set :rack_env, :production
 
