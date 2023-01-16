d8225835236e5603bb7e36450ef68f3b1cfafffa
Jonathan Chapman <chapmajs@gmail.com>
2023-01-16 18:47:47 -0500

    Fixing deprecation warning in User validation

--- models/user.rb
+++ models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
 
   has_many :resource_records
 
-  validates :username, :presence => true, :uniqueness => true
+  validates :username, :presence => true, :uniqueness => { :case_sensitive => true }
 end
